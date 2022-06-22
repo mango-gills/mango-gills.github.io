e778d91a22d61df5fa0b43fb2a6f34a59cf70208
mango_gills <juzzgil21@gmail.com>
2022-06-22 16:39:37 +0800

    added scnap scroll and return to top button

--- script/script.js
+++ script/script.js
@@ -1,18 +1,19 @@
 const navbar = document.querySelector(".nav-bar");
+const scrollToTopButton = document.querySelector(".scrollToTop");
 
 let lastScrollToTop;
 
 window.addEventListener("scroll", () => {
   let scrollToTop = window.pageYOffset || document.documentElement.scrollTop;
   if (scrollToTop > lastScrollToTop) {
-    navbar.style.top = "-80px";
+    scrollToTopButton.style.bottom = "50px";
   } else {
-    navbar.style.top = "0";
+    scrollToTopButton.style.bottom = "-50px";
   }
   lastScrollToTop = scrollToTop;
 });
 
-// window.scrollTo({
-//   top: 0,
-//   behavior: "smooth",
-// });
+// return to the top on button click
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
