395bd8d5f716fd6e87adb75ebf3152f55d06cf1b
mango_gills <juzzgil21@gmail.com>
2022-06-27 18:08:34 +0800

    add project details in modal

--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@ const closeModal = document.querySelector(".close-modal");
 const projectContainer = document.querySelectorAll(".project-container");
 const projectTitle = document.querySelector(".project-title");
 const modalImage = document.getElementById("modal-image");
+const modalText = document.querySelector(".modal-text");
 
 let rootElement = document.documentElement;
 
@@ -36,11 +37,25 @@ closeModal.addEventListener("click", () => {
 });
 
 projectContainer.forEach((project) => {
-  // console.log(project.getElementsByTagName("img"));
+  console.log(project);
   project.addEventListener("click", () => {
     modal.style.visibility = "visible";
     projectTitle.innerText = project.getElementsByTagName("h1")[0].innerText;
     modalImage.src = project.getElementsByTagName("img")[0].src;
+
+    if (project.getElementsByTagName("h1")[0].classList.contains("static")) {
+      modalText.innerText = descriptions.static;
+    } else if (
+      project.getElementsByTagName("h1")[0].classList.contains("dynamic")
+    ) {
+      modalText.innerText = descriptions.dynamic;
+    } else if (
+      project.getElementsByTagName("h1")[0].classList.contains("fullstack")
+    ) {
+      modalText.innerText = descriptions.fullstack;
+    } else {
+      modalText.innerText = descriptions.final;
+    }
   });
 });
 
@@ -48,4 +63,15 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.visibility = "hidden";
   }
+};
+
+//
+const descriptions = {
+  static: "A static web portfolio made with HTML & CSS.",
+  dynamic:
+    "Who wants to be a millionaire web game made in HTML, CSS and JavaScript. First team collaboration.",
+  fullstack:
+    "Food blogging web app, with CRUD functionality made with MERN Stack.",
+  final:
+    "Chat application using ReactJS for front-end and Firebase for the backend. Features sending text and images, searching and adding users.",
 };
